00f14c432c2f3e7e239b909879d59088acb3feb3
WoolMike <150397724+WoolMike@users.noreply.github.com>
2024-03-04 22:12:05 +0000

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let first = ["the", "all", "best", "mind"];
+  let second = ["setter", "broker", "gamer", "goat"];
+  let third = ["player", "beter", "camper"];
+  let dot = [".com", ".net", ".mx", "es"];
+  for (let i = 0; i <= 10; i++) {
+    let randomFirst = first[Math.floor(Math.random() * first.length)];
+    let randomSecond = second[Math.floor(Math.random() * second.length)];
+    let randomThird = third[Math.floor(Math.random() * third.length)];
+    let randomDot = dot[Math.floor(Math.random() * dot.length)];
+
+    let dominio = randomFirst + randomSecond + randomThird + randomDot;
+
+    document.getElementById("domain").innerHTML = dominio;
+
+    console.log(dominio);
+  }
+
   console.log("Hello Rigo from the console!");
 };
